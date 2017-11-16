437ef04a2f9a83c7e9e899c68881e71a1304caad
nagarjunrv <33398889+nagarjunrv@users.noreply.github.com>
2017-11-15 23:31:00 -0500

    dictionary of all words
    a dictionary file with all possible words is created

---
+++ main.py
@@ -0,0 +1,3 @@
+import shingling
+
+print (shingling.wordShingles("the white brown is good and a bit", 3))
--- parsing.py
+++ parsing.py
@@ -4,9 +4,10 @@ Created on Sun Nov  5 13:24:17 2017
 
 @author: Nagarjun
 """
-"""
+
 from bs4 import BeautifulSoup
 import os
+from string import punctuation
 input_directory = "dataset"
 input_string = ""
 preprocessed_input = {}
@@ -27,13 +28,22 @@ for text in all_text:
     text_str = " ".join(text_str[:text_str.rfind("Reuter")].split())
     preprocessed_input[count] = text_str
     count += 1
-"""
 
+word_dict = set()
 final_pre_processed_str = ""
 for _, value in preprocessed_input.items():
-    final_pre_processed_str += value + '\n'
-    
+	value = value.lower()
+	trans_table = value.maketrans('', '', punctuation)
+	value = value.translate(trans_table)
+	word_dict.update(value.split())
+	final_pre_processed_str += value + '\n'
 file = open("final_pre_processed.txt", 'w')
 file.write(final_pre_processed_str)
+file.close()
+
+
+file = open("word_dictionary.txt", 'w')
+for item in word_dict:
+	file.write("%s\n" % item)
 file.close()
     
